52a28e3fe38525567d80aabee930672177e07116
JOSHUA RODGERS <joshuaridgers986@gmail.com>
2025-09-02 04:27:01 +0300

    urls changed to swith to render urls gain for the second time

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -9,7 +9,7 @@ document.getElementById('loginForm').addEventListener('submit', async function (
 
     try {
         //Send login request
-        const response = await fetch("http://localhost:8080/api/auth/login", {
+        const response = await fetch("https://uvmsapiv1.onrender.com/api/auth/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(body)
